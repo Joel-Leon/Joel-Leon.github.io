f8f9b4f45b5e429e564841bec9c50da3923b1e72
Joel Leon M <83482069+Joel-Leon@users.noreply.github.com>
2022-10-24 09:02:23 -0500

    Add files via upload

--- Andrea'sTrunk/NewVersion/evaluaciones/js/code.js
+++ Andrea'sTrunk/NewVersion/evaluaciones/js/code.js
@@ -7,8 +7,8 @@
 
     /* List Assessment */
 
-    setAssessment(mssgAvailable(`23/10/22`,`11:00 p.m.`, `24/10/22.`), "https://forms.office.com/r/rsfviTx20Q");
-    setAssessment(mssgAvailable(`##/##/##`,`##:## p.m.`, `##:## p.m.`), "#");
+    setAssessment(mssgAvailable(`24/10/22`,`9:00 a.m.`, `28/10/22.`), "https://forms.office.com/r/rsfviTx20Q");
+    setAssessment(mssgAvailable(`28/10/22`,`9:00: a.m.`, `04/11/22.`), "https://forms.office.com/r/Pp16dSF6Yx");
     setAssessment(mssgAvailable(`##/##/##`,`##:## p.m.`, `##:## p.m.`), "#");
     setAssessment(mssgAvailable(`##/##/##`,`##:## p.m.`, `##:## p.m.`), "#");
     setAssessment(mssgSoonAvailable);
